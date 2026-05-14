889c7167028fd9075726c1f56de3d46086123e6e
palzadafiya <116303922+palzadafiya@users.noreply.github.com>
2026-05-14 21:18:12 +0530

    vercel modification

--- index.js
+++ index.js
@@ -45,8 +45,9 @@ app.get('/', (req, res) => {
 // Error handling middleware
 app.use(errorHandler);
 
+module.exports = app;
 // Start server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
-}); 
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => {
+//   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+// }); 
